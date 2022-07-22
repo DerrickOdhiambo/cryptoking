fbdb1531a81365239135cfbacc4a9b9f6aa761cd
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-07-23 00:56:18 +0300

    import cryto hook for fetching data from the api, and also import millify to format large numbers from the api call

--- src/components/data/Data.jsx
+++ src/components/data/Data.jsx
@@ -1,31 +1,39 @@
 import React from 'react';
+import { useGetCryptosQuery } from '../../services/cryptoApi';
+import millify from 'millify';
 
 import './data.scss';
 
 const Data = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+  console.log(data);
+  if (isFetching) return 'Loading';
+
+  const globalStats = data?.data?.stats;
+
   return (
     <div className='data__visualization'>
       <h1>Global crypto stats</h1>
       <div className='data__stats'>
         <div className='data'>
           <p>Total cryptocurrencies</p>
-          <span>5</span>
+          <span>{globalStats.total}</span>
         </div>
         <div className='data'>
           <p>total exchanges</p>
-          <span>5</span>
+          <span>{globalStats.totalExchanges}</span>
         </div>
         <div className='data'>
           <p>total market cap</p>
-          <span>5</span>
+          <span>{millify(globalStats.totalMarketCap)}</span>
         </div>
         <div className='data'>
-          <p>total 24th volume</p>
-          <span>5</span>
+          <p>total 24h volume</p>
+          <span>{millify(globalStats.total24hVolume)}</span>
         </div>
         <div className='data'>
           <p>total markets</p>
-          <span>5</span>
+          <span>{millify(globalStats.totalMarkets)}</span>
         </div>
       </div>
     </div>
