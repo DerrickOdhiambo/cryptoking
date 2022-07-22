558af7b2783a7d2e77595443cd51b7e02f3bdc1f
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-07-23 00:41:04 +0300

    create api service using redux and RTK Query to fetch data from api endpoint

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'X-RapidAPI-Key': '3044591d67msh1b286c7d048b314p107089jsn623dab1d2f99',
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/coins'),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
