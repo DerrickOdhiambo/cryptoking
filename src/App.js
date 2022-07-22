542766c2802b772124e7fdb26d17959a90af63f5
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-07-22 13:40:27 +0300

    add new routes to the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cryptocurrencies from './pages/Cryptocurrencies';
+import Exchanges from './pages/Exchanges';
 import Home from './pages/Home';
+import News from './pages/News';
 import './styles/App.scss';
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
       <div className='App'>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/exhanges' element={<Exchanges />} />
+          <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+          <Route path='/news' element={<News />} />
         </Routes>
       </div>
     </Router>
