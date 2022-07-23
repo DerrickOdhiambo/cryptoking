a8e8a76aba9c54ea0ecd7871bcbb70df5dcfd89b
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2022-07-23 13:44:07 +0300

    import components created to their specific routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Cryptocurrencies from './pages/Cryptocurrencies';
+import Cryptocurrencies from './components/crypto/Cryptocurrencies';
 import Exchanges from './pages/Exchanges';
 import Home from './pages/Home';
-import News from './pages/News';
+import News from './components/news/News';
+import Header from './components/navbar/Header';
+
 import './styles/App.scss';
 
 function App() {
   return (
     <Router>
       <div className='App'>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/exchanges' element={<Exchanges />} />
+          <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
           <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
           <Route path='/news' element={<News />} />
         </Routes>
